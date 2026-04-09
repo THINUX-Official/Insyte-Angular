97fce882782ce0c7a8c29b43c72888bbfcffe543
thinushad <thinusha.d@arpicoinsurance.com>
2026-04-09 14:57:54 +0530

    create initial login and auth functions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {Login} from './auth/login/login';
+import {AdminDashboard} from './features/dashboards/admin/admin-dashboard/admin-dashboard';
+import {roleGuard} from './core/guards/role.guard';
+import {Unauthorized} from './features/dashboards/unauthorized/unauthorized';
+import {authGuard} from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {path: '', component: Login},
-  {path: 'login', component: Login},
+  {
+    path: 'admin',
+    component: AdminDashboard,
+    canActivate: [authGuard, roleGuard],
+    data: {roles: ['ADMIN']},
+  },
+  {path: 'unauthorized', component: Unauthorized}
 ];
--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators,} from '@angular/forms';
+import {environment} from '../../../environments/environment';
+import {MatFormField, MatInput, MatLabel, MatSuffix} from '@angular/material/input';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {MatCheckbox} from '@angular/material/checkbox';
+import {Router} from '@angular/router';
+import {Auth} from '../../core/services/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatIconButton,
+    MatSuffix,
+    MatIcon,
+    MatCheckbox,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
+  standalone: true
 })
 export class Login {
 
+  version = environment.version;
+  signInForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    rememberMe: new FormControl(false),
+  });
+  hide = true;
+
+  constructor(private authService: Auth, private router: Router) {
+  }
+
+  forgetPassword() {
+
+  }
+
+  login(): void {
+    if (this.signInForm.invalid) {
+      this.signInForm.markAllAsTouched();
+      return;
+    }
+
+    const payload = {
+      username: this.signInForm.value.username ?? '',
+      password: this.signInForm.value.password ?? '',
+      rememberMe: this.signInForm.value.rememberMe ?? false,
+    };
+
+    this.authService.login(payload).subscribe({
+      next: (response) => {
+        this.authService.storeSession(response);
+        const route = this.authService.getDashboardRoute(response.roles);
+        this.router.navigate([route]);
+      }, error: (err) => {
+        console.error('Login failed', err);
+      }
+    })
+  }
 }
