b8439dc0416e684cc61aa470d3dd43eab43b5688
thinushad <thinusha.d@arpicoinsurance.com>
2026-04-20 17:59:03 +0530

    add sample chart to admin dashboard

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,31 @@ import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './core/interceptors/auth.interceptor';
 
+import {provideEchartsCore} from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+
+import {BarChart, LineChart, PieChart} from 'echarts/charts';
+import {DatasetComponent, GridComponent, LegendComponent, TitleComponent, TooltipComponent,} from 'echarts/components';
+import {CanvasRenderer} from 'echarts/renderers';
+
+// Register chart types and components globally
+echarts.use([
+  BarChart,
+  LineChart,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent,
+  DatasetComponent,
+  CanvasRenderer,
+]);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideEchartsCore({echarts}),
   ],
 };
